ed917212d0611eee4e7fee56637ecd9cb0a401aa
Jinit <jinitvyas0107@gmail.com>
2024-03-02 07:36:09 +0530

    summary feature add

--- Home/views.py
+++ Home/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest
 from translate import Translator
 
+# from sumy.parsers.plaintext import PlaintextParser
+# from sumy.nlp.tokenizers import Tokenizer
+# from sumy.summarizers.lsa import LsaSummarizer
+# import nltk
+
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer
+from sumy.summarizers.text_rank import TextRankSummarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
 
 # Create your views here.
 def index(request):
@@ -24,9 +33,44 @@ def to_OutputLang(request):
 
         if output_lang and input_text:
             translator = Translator(output_lang)
-            translation_in_output_lang = translator.translate(to_eng(input_text))
+            translation_in_output_lang = translator.translate(summarize_text(to_eng(input_text), num_sentences = request.POST.get('numOfLines')))
             return HttpResponse(translation_in_output_lang)
         else:
+            print("Missing input text or output language.")
             return HttpResponseBadRequest("Missing input text or output language.")
     else:
+        print("Invalid request method.")
         return HttpResponseBadRequest("Invalid request method.")
+
+# def summarize_text(text, num_sentences=2):
+    # Initialize parser and tokenizer
+    # parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    
+    # # Initialize LSA Summarizer
+    # summarizer = LsaSummarizer()
+    
+    # # Summarize the text
+    # summary = summarizer(parser.document, num_sentences)
+    
+    # # Join the summarized sentences into a single string
+    # summarized_text = " ".join([str(sentence) for sentence in summary])
+    
+    # return summarized_text
+
+def summarize_text(text, num_sentences=3, summarization_model='LSA'):
+    # Preprocess the input text if necessary
+
+    # Create a parser object
+    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+
+    # Initialize the summarization model
+    if summarization_model == 'LSA':
+        summarizer = LsaSummarizer(Stemmer("english"))
+
+    # Summarize the text
+    summary = summarizer(parser.document, num_sentences)
+
+    # Join the summarized sentences into a single string
+    summarized_text = " ".join([str(sentence) for sentence in summary])
+
+    return summarized_text
